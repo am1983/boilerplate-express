e812946951753793dbdbe100cce5766eea9ab280
Aaron Marks <me@aaronmarks.dev>
2021-03-04 16:25:06 -0500

    Setup middleware for static content

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// Mount the express.static() middleware to the path /public.
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
     let path = __dirname + '/views/index.html';
     res.sendFile(path);
