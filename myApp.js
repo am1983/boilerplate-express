515fb2f438704ab8b0389589a4426e281732adc0
Aaron Marks <me@aaronmarks.dev>
2021-03-05 15:37:22 -0500

    Finally got dotenv working

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 // Mount the express.static() middleware to the path /public.
 app.use("/public", express.static(__dirname + "/public"));
@@ -10,14 +11,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/json', (req, res) => {
-    var data = {
-        "message": "Hello json"
-    };
+
+    var message = "Hello json";
 
     if (process.env.MESSAGE_STYLE==='uppercase')
     {
-        data.message.toUpperCase();
+        message = message.toUpperCase();
     }
+
+    var data = {
+        "message": message
+    };
 
     res.json(data);
 });
